2cfbd362cd775f4cebd3d844245c5a696dd6d756
Hsi-Shou Wu <hsiwu@umich.edu>
2015-12-06 18:18:38 -0500

    delete Test.py

--- Test.py
+++ Test.py
@@ -1,9 +1,10 @@
-
+from util import load_url
+data = load_url()
 
 import numpy as np
-from StochasticGradient import SG
+from cw import cw
 
-sg = SG()
+cw_xtest = cw()
 
 day1X = np.matrix([[1,0,1,1,0], [0,0,1,0,0]])
 day1Y = np.matrix([1, 0])
@@ -12,7 +13,9 @@ day1Y = np.matrix([1, 0])
 
 # print day1X[0, :]
 # print day1Y[:,1]
-data = {0: {'data':day1X, 'labels':day1Y},1: {'data':day1X, 'labels':day1Y}, 2: {'data':day1X, 'labels':day1Y}}
-(cumuErrorLs, cumuFPLs) = sg.calculate(7, 3, data)
+#data = {0: {'data':day1X, 'labels':day1Y},1: {'data':day1X, 'labels':day1Y}, 2: {'data':day1X, 'labels':day1Y}}
+eta = 0.95
+sigma_format = 'diag'
+(cumuErrorLs, cumuFPLs) = cw_xtest.calculate( 3, data, eta, sigma_format)
 
 print (cumuErrorLs, cumuFPLs)
