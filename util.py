8228711790fde49c94999dec2aa6d94252042c57
En Shuo Hsu <jimmy99888@gmail.com>
2015-12-06 18:06:34 -0500

    change labels

--- util.py
+++ util.py
@@ -1,9 +1,12 @@
 import scipy.io as sio
+import numpy as np
 
 
 def load_url(path='url.mat'):
     url = sio.loadmat(path)
     n_url = {i: {'data': url['Day%d' % i]['data'][0][0],
-             'labels': url['Day%d' % i]['labels'][0][0]}
+             'labels': url['Day%d' % i]['labels'][0][0].astype(np.int8)}
              for i in range(120)}
+    for i in range(120):
+        np.place(n_url[i]['labels'], n_url[i]['labels'] == 0, -1)
     return n_url
